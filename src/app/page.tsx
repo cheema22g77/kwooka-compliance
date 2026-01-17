cc4f8bc5dbc29b49f319ea015779fbb3bb031772
Baljeet Cheema <cheema@kwooka.com.au>
2026-01-17 18:57:30 +0800

    Add root redirect to login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import { redirect } from 'next/navigation'
 
 export default function Home() {
-  // In production, you'd check auth state here
-  // For now, redirect to login
   redirect('/auth/login')
 }
